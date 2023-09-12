2a39e667a8fda1541d517c16397186930481aa5e
ltz <lantian.zhang@mail.utoronto.ca>
2023-09-11 22:59:34 -0400

    print non-negative multiples of 3 or 5

---
+++ src/Multiples.java
@@ -0,0 +1,23 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 1000) {
+
+            i = doFizzBuzz(i);
+        }
+    }
+
+    public static int doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 || divisibleBy5) {
+
+            System.out.println(i);
+        }
+        i++;
+        return i;
+    }
+}
